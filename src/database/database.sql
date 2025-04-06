920ce0dabc62cdc165e1ed3e21b256bf10169233
atommnava <atommunoznava@gmail.com>
2025-04-06 09:54:59 -0600

    Cambios desde remoto

--- src/database/database.sql
+++ src/database/database.sql
@@ -12,6 +12,6 @@ CREATE TABLE books (
 );
 
 INSERT INTO books (title, author, genre, price, stock, cover_image) VALUES
-('El Principito', 'Antoine de Saint-Exupéry', 'Ficción', 12.99, 10, 'covers/principito.png'),
-('Cien Años de Soledad', 'Gabriel García Márquez', 'Realismo Mágico', 15.50, 5, 'covers/cien_anios.png'),
-('1984', 'George Orwell', 'Distopía', 14.99, 8, 'covers/1984.png');
+('El Principito', 'Antoine de Saint-Exupéry', 'Ficción', 12.99, 10, 'public/covers/principito.png'),
+('Cien Años de Soledad', 'Gabriel García Márquez', 'Realismo Mágico', 15.50, 5, 'public/covers/cien_anios.png'),
+('1984', 'George Orwell', 'Distopía', 14.99, 8, 'public/covers/1984.png');
