a4ba7f7d054a29d6cbad962f27fa9d427a30cf9a
atommnava <atommunoznava@gmail.com>
2025-04-10 20:56:51 -0600

    BACKEND - Commit desde remoto

--- src/login_register.php
+++ src/login_register.php
@@ -12,6 +12,10 @@
 session_start();
 require_once 'config.php';
 
+/*
+ * We need to create the condition 
+ * to handle the REGISTRATION process
+ */
 if (isset($_POST['register'])) {
     $name = $_POST['name'];
     $email = $_POST['email'];
@@ -25,6 +29,35 @@ if (isset($_POST['register'])) {
     } else {
         $conn -> query("INSERT INTO users(name, email, password, role) VALUES('$name', '$email', '$password', '$role')");
     }
+    header("Location: index2.php");
+    exit();
+}
+
+/*
+ * We need to create another condition 
+ * to handle the LOGIN process
+ */
+if (isset($_POST['login'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    
+    $result = $conn -> query("SELECT * FROM users WHERE email = '$email'");
+    if ($result -> num_rows > 0) {
+        $user = $result -> fetch_assoc();
+        if (password_verify($password, $user['password'])) {
+            $_SESSION['name'] = $user['name'];
+            $_SESSION['email'] = $user['email'];
+
+            if ($user['role'] === 'admin') {
+                header("Location: admin_page.php");
+            } else {
+                header("Location: user_page.php");
+            }
+            exit();
+        }
+    }
+    $_SESSION['login_error'] = 'Incorrect email or password';
+    $_SESSION['active_form'] = 'login';
     header("Location: index2.php");
     exit();
 }
